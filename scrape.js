a25dbb44c103abaea53b7c66f63dca80b129404d
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-08-19 08:52:27 +0200

    feat: add number of donations

--- scrape.js
+++ scrape.js
@@ -9,13 +9,15 @@ fetch(`https://donorbox.org/${campaign}`)
     const jsdom = new JSDOM(page);
     const { window } = jsdom;
     const totalRaised = window.document.getElementById("total-raised");
+    const numDonations = window.document.getElementById("paid-count");
     const panel1 = window.document.getElementById("panel-1");
     const goal = panel1.getElementsByClassName("description")[0].children[2]
       .children[0];
     process.stdout.write(
       JSON.stringify({
-        totalRaised: parseInt(totalRaised.textContent.replace(/[^0-9]/g, "")),
+        raised: parseInt(totalRaised.textContent.replace(/[^0-9]/g, "")),
         goal: parseInt(goal.textContent.replace(/[^0-9]/g, "")),
+        donations: parseInt(numDonations.textContent),
       })
     );
   });
